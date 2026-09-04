04f0965012cb18a89d1c0a9281caec429bc0b1c0
CedricLec05 <e2235445@cmaisonneuve.qc.ca>
2026-09-04 12:50:43 -0400

    1. Je n'ai rien fait de spectaculaire boss 2. Désolé 3. Meilleur chance la prochaine fois

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-  // Inclue le fichier commun contenant le haut du code des écrans de UI
-  include_once('commun/entete.inc.php');
+// Inclue le fichier commun contenant le haut du code des écrans de UI
+include_once('commun/entete.inc.php');
 ?>
 <main class="page-accueil">
   <article class="amorce">
@@ -24,6 +24,6 @@
   </article>
 </main>
 <?php
-  // Inclue le fichier commun contenant le bas du code des écrans de UI
-  include_once('commun/p2p.inc.php');
+// Inclue le fichier commun contenant le bas du code des écrans de UI
+include_once('commun/p2p.inc.php');
 ?>
